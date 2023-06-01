0abc0fc1b7a6b18c36218f1b600e50aed0f33e63
Mirze26 <mirzaab@code.edu.az>
2023-06-01 20:35:07 +0400

    Slider completed

---
+++ End Project/assets/js/Home/home.js
@@ -0,0 +1,18 @@
+
+
+let swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesToScroll: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+
+});
