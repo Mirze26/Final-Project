f8dcb212d79f21a7faac307b33b36ada50c95461
Mirze26 <mirzaab@code.edu.az>
2023-06-27 16:38:06 +0400

    filter button added

--- End Project/assets/js/Wishlist/wishlist.js
+++ End Project/assets/js/Wishlist/wishlist.js
@@ -1,4 +1,21 @@
-
+$(".remove").click(function () {
+  var el = $(this);
+  el.parent().parent().addClass("removed");
+  window.setTimeout(
+    function () {
+      el.parent().parent().slideUp('fast', function () {
+        el.parent().parent().remove();
+        if ($(".product").length == 0) {
+          if (check) {
+            $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
+          } else {
+            $("#cart").html("<h1 class='empty'>No products!</h1>");
+          }
+        }
+        changeTotal();
+      });
+    }, 200);
+});
 
 
 let userIcon = document.querySelector("#app-menu .icons ul li .user")
